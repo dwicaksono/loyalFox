25405320b53d5de92996237f18a4eee032c0baff
dimas wicaksono <novan.top@gmail.com>
2019-11-05 14:09:36 +0700

    :pencil:

--- phase-0/week-04/02 selasa week-04/modus.js
+++ phase-0/week-04/02 selasa week-04/modus.js
@@ -1,17 +1,39 @@
 function cariModus(arr) {
   // you can only write your code here!
+  // var maxCount = 0;
+  // var modus = 0;
+  // for (var i = 0; i < arr.length; i++) {
+  //   var count = 0;
+  //   for (var j = 0; j < arr.length; j++) {
+  //     if (arr[i] === arr[j]) count++;
+  //   }
+  //   if (count > maxCount) modus = arr[i];
+  //   maxCount = count;
+  // }
+  // maxCount
+  // return maxCount === 1 || maxCount === arr.length ? -1 : modus;
+
   var maxCount = 0;
   var modus = 0;
-  for (var i = 0; i < arr.length; i++) {
+  for (var x = 0; x < arr.length; x++) {
     var count = 0;
-    for (var j = 0; j < arr.length; j++) {
-      if (arr[i] === arr[j]) count++;
+    for (var y = 0; y < arr.length; y++) {
+      if (arr[x] === arr[y]) {
+        count++;
+      }
     }
-    if (count > maxCount) modus = arr[i];
-    maxCount = count;
+    if (count > maxCount) {
+      modus = arr[x];
+      maxCount = count;
+    }
   }
-  return maxCount === 1 || maxCount === arr.lengt ? -1 : modus;
+  if (maxCount === 1 || maxCount === arr.length) {
+    return -1;
+  } else {
+    return modus;
+  }
 }
+
 // TEST/ CASES
 console.log(cariModus([10, 4, 5, 2, 4])); // 4
 console.log(cariModus([5, 10, 10, 6, 5])); // 5
